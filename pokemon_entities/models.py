107247eb5b468b63e698391ee7b043f4a7107025
Konstantin <akechijinsai@gmail.com>
2022-02-12 15:55:29 +0300

    Fix field options and types in models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,12 +2,12 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(verbose_name='Имя (RU)',max_length=200)
-    title_en = models.CharField(verbose_name='Имя (EN)', max_length=200, null=True)
-    title_jp = models.CharField(verbose_name='Имя (JP)', max_length=200, null=True)
-    description = models.TextField(verbose_name='Описание', default='Описание покемона')
+    title = models.CharField(verbose_name='Имя (RU)', max_length=200)
+    title_en = models.CharField(verbose_name='Имя (EN)', max_length=200, blank=True)
+    title_jp = models.CharField(verbose_name='Имя (JP)', max_length=200, blank=True)
+    description = models.TextField(verbose_name='Описание', default='', blank=True)
     image = models.ImageField(verbose_name='Изображение', blank=True)
-    evolution_from = models.ForeignKey('Pokemon',
+    evolution_from = models.ForeignKey('self',
                                        related_name='next_evolutions',
                                        null=True,
                                        blank=True,
@@ -23,8 +23,8 @@ class PokemonEntity(models.Model):
                                 related_name='pokemons',
                                 null=True,
                                 verbose_name='Покемон')
-    lat = models.FloatField(verbose_name='Широта', null=True)
-    lon = models.FloatField(verbose_name='Долгота', null=True)
+    lat = models.FloatField(verbose_name='Широта')
+    lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(verbose_name='Время появления', null=True, blank=True)
     disappeared_at = models.DateTimeField(verbose_name='Время исчезновения', null=True, blank=True)
     level = models.IntegerField(verbose_name='Уровень', null=True, blank=True)
